190f504e9552922a7023af88ce992d0ec25c3894
Stefan Micheelsen <stufkan@gmail.com>
2016-01-27 22:39:53 +0100

    too big commit :(#

--- src/com/stufkan/grocer/Second.java
+++ src/com/stufkan/grocer/Second.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,144 +15,136 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 
-public class Second extends Activity implements OnClickListener{
+public class Second extends Activity implements OnClickListener {
 
-	public enum sortMode{Alphabetically, Frequency}
-	public static final String PREFS_NAME = "GrocerPrefsFile";
-	
-	MySQLiteHelper db;
-	TextView tv;
-	ListView lv;
-	ItemAdapter adapter;
-	sortMode sMode;
+    public enum sortMode {Alphabetically, Frequency}
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    public static final String PREFS_NAME = "GrocerPrefsFile";
+
+    MySQLiteHelper db;
+    TextView tv;
+    ListView lv;
+    ItemAdapter adapter;
+    sortMode sMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         db = new MySQLiteHelper(this);
-        adapter = new ItemAdapter(this,R.layout.grocerlistviewitem,db.getAllItems(sMode));
+        adapter = new ItemAdapter(this, R.layout.grocerlistviewitem, db.getAllItems(sMode));
 
-		SharedPreferences settings = getPreferences(MODE_PRIVATE);
-		sMode = sortMode.valueOf(settings.getString("sortMode", "Alphabetically"));
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        sMode = sortMode.valueOf(settings.getString("sortMode", "Alphabetically"));
 
-		setContentView(R.layout.second);
-
-
-
-		Button add = (Button) findViewById(R.id.AddNewItem);
-		add.setOnClickListener(this);
-
-		Button deleteItems = (Button) findViewById(R.id.DeleteItems);
-		deleteItems.setOnClickListener(this);
-
-		Button addItems = (Button) findViewById(R.id.AddItems);
-		addItems.setOnClickListener(this);
-
-		tv =  (TextView) findViewById(R.id.editText1);
-
-		lv = (ListView) findViewById(R.id.listView1);
-
-		lv.setAdapter(adapter);
-		lv.setTextFilterEnabled(true);
-
-		tv.addTextChangedListener(filterTextWatcher);
-
-	}
-
-	private TextWatcher filterTextWatcher = new TextWatcher() {
-
-		public void onTextChanged(CharSequence s, int start, int before, int count) {
-			adapter.getFilter().filter(s);
-			//adapter.notifyDataSetChanged();
-		}
-
-		public void beforeTextChanged(CharSequence s, int start, int count,
-				int after) {
+        setContentView(R.layout.second);
 
 
-		}
+        Button add = (Button) findViewById(R.id.AddNewItem);
+        add.setOnClickListener(this);
 
-		public void afterTextChanged(Editable s) {
+        Button deleteItems = (Button) findViewById(R.id.DeleteItems);
+        deleteItems.setOnClickListener(this);
 
-		}
-	};
+        Button addItems = (Button) findViewById(R.id.AddItems);
+        addItems.setOnClickListener(this);
+
+        tv = (TextView) findViewById(R.id.editText1);
+
+        lv = (ListView) findViewById(R.id.listView1);
+
+        lv.setAdapter(adapter);
+        lv.setTextFilterEnabled(true);
+
+        tv.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                Log.d("textwatcher", "ontextchanged " + s.toString());
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+
+                Log.d("textwatcher", "beforetextchanged " + s.toString());
+            }
+
+            public void afterTextChanged(Editable s) {
+
+                Log.d("textwatcher", "after " + s.toString());
+
+                adapter.getFilter().filter(s);
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
 
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.second, menu);
-		return true;
-	}
+        switch (item.getItemId()) {
+            case R.id.menu_SortFrequency:
+                sMode = sortMode.Frequency;
+                resetList();
+                break;
+            case R.id.menu_sortAlpha:
+                sMode = sortMode.Alphabetically;
+                resetList();
+                break;
+        }
+        editor.putString("sortMode", sMode.toString());
+        editor.commit();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		SharedPreferences settings = getPreferences(MODE_PRIVATE);
-		SharedPreferences.Editor editor = settings.edit();
+        return super.onOptionsItemSelected(item);
+    }
 
-		
-		switch(item.getItemId())
-		{
-		case R.id.menu_SortFrequency:
-			sMode = sortMode.Frequency;
-			resetList();
-			break;
-		case R.id.menu_sortAlpha:
-			sMode = sortMode.Alphabetically;
-			resetList();
-			break;
-		}
-		editor.putString("sortMode", sMode.toString());
-		editor.commit();
+    public void onClick(View v) {
+        if (v.getId() == R.id.AddNewItem) {
+            int tvText = tv.getText().length();
+            if (tvText != 0)
+                db.addItemCurrent(new Item(tv.getText().toString()));
+            resetList();
+            //adapter.data.get(Collections.binarySearch(items, new Item(tv.getText().toString()), Item.NameComparator)).
+            //forsøg på at få tilføjede elementer til at være "selected" fra starten
 
-		return super.onOptionsItemSelected(item);
-	}
+            tv.setText("");
+        } else if (v.getId() == R.id.AddItems) {
+            for (Item i : adapter.data) {
+                if (i.getSelected() == true) {
+                    i.incrementTimesBought();
+                    db.AvailableToCurrent(i);
+                }
+            }
+            resetList();
+            tv.setText("");
+        } else if (v.getId() == R.id.DeleteItems) {
+            for (Item i : adapter.data) {
+                if (i.getSelected() == true)
+                    db.deleteItemAvailable(i);
+            }
+            resetList();
+            tv.setText("");
+        }
+    }
 
-	public void onClick(View v) {
-		if(v.getId() == R.id.AddNewItem)
-		{
-			int tvText = tv.getText().length();
-			if(tvText != 0)
-				db.addItemCurrent(new Item(tv.getText().toString()));
-			resetList();
-			//adapter.data.get(Collections.binarySearch(items, new Item(tv.getText().toString()), Item.NameComparator)).
-			//forsøg på at få tilføjede elementer til at være "selected" fra starten
-			
-			tv.setText("");
-		}
-		else if(v.getId() == R.id.AddItems)
-		{
-			for (Item i : adapter.data) {
-				if(i.getSelected() == true)
-				{
-					i.incrementTimesBought();
-					db.AvailableToCurrent(i);
-				}
-			}
-			resetList();
-			tv.setText("");
-		}
-		else if(v.getId() == R.id.DeleteItems)
-		{
-			for (Item i : adapter.data) {
-				if(i.getSelected()== true)
-					db.deleteItemAvailable(i);	
-			}
-			resetList();
-			tv.setText("");
-		}
-	}
-
-	private void resetList()
-	{
+    private void resetList() {
         adapter.data = db.getAllItems(sMode);
-		adapter = new ItemAdapter(this,R.layout.grocerlistviewitem,adapter.data);
-		lv.setAdapter(adapter);
-	}
+        adapter = new ItemAdapter(this, R.layout.grocerlistviewitem, adapter.data);
+        lv.setAdapter(adapter);
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		tv.removeTextChangedListener(filterTextWatcher);
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
